7c5eff1d8219a950438797d7a6c931d72317fc8a
SymphonyIceAttack <1994370030@qq.com>
2023-02-13 00:41:10 +0800

    customNanoid

--- src/router/EquipMentEdit/EquipMentCrypto/index.ts
+++ src/router/EquipMentEdit/EquipMentCrypto/index.ts
@@ -1,8 +1,9 @@
 import CryptoJS from "crypto-js";
 import Router from "koa-router";
 import { PrismaClient } from "@prisma/client";
-import { nanoid } from "nanoid";
+import { customAlphabet } from "nanoid";
 const prisma = new PrismaClient();
+const nanoid = customAlphabet("QWERTYUIOPASDFGHJKLZXCVBNM123456789", 8);
 export const SecretKey =
     "79540473-df1c-476a-ac85-4994a7a71af6-27747e3c-fa20-4bd4-b9d9-1427331f006c";
 export const EncryptEquipMent = new Router();
